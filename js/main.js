ce40747558a0e86294f59d125980385c7be5bc9e
vladqwer <epifanov.vld@yandex.ru>
2016-07-19 09:49:23 +0300

    comit test1

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var startTime;
+var checkTime;
+
+//Initialize function
+var init = function () {
+	// TODO:: Do your initialization job
+	console.log("init() called");
+
+	// add eventListener for tizenhwkey
+	document.addEventListener('tizenhwkey', function(e) {
+		if(e.keyName == "back") {
+			try {
+				tizen.application.getCurrentApplication().exit();
+			} catch (error) {
+				console.error("getCurrentApplication(): " + error.message);
+			}
+		}
+	});
+};
+// window.onload can work without <body onload="">
+window.onload = init;
+
+
+
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('divbutton1').innerHTML="Current time: " + h + ":" + m + ":" + s;
+	var t = setTimeout(startTime, 250);
+}
+
+
+var vis=true;
+function dopic() {
+	var img = document.getElementById('pic1');
+	if(vis){img.style.visibility='hidden';}else {img.style.visibility='visible';}vis=!vis;
+    
+}
+
+
+function screenfunk() {
+	var w=window;
+	document.getElementById('divbutton3').innerHTML="screen info: width="+w.screen.width+"   height:"+w.screen.height;
+    
+}
+
+
+function checkTime(i) {
+	if (i < 10) {
+		i="0" + i;
+	}
+	return i;
+}
